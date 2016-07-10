cf66e3126c54c3b163265cf1b0e74ef2e8ba222e
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-07-10 14:01:13 +0900

    add make10

---
+++ main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import itertools
+
+ops = ["+", "-", "*", "/"]
+nums = input().split()
+length = len(nums) - 1
+patterns = range(length)
+
+
+class Value:
+    def __init__(self, *fom):
+        self.fom = fom
+
+    def __str__(self):
+        return "(%s)" % " ".join(map(str, self.fom))
+
+
+def formula(ops, nums, patterns):
+    ops = list(ops)
+    nums = list(nums)
+    patterns = list(patterns)
+    while patterns:
+        target = min(patterns)
+        index = patterns.index(target)
+        value = Value(nums[index], ops[index], nums[index + 1])
+        nums.pop(index)
+        nums[index] = value
+
+        ops.pop(index)
+        patterns.pop(index)
+    return str(nums[0])
+
+
+result = []
+for n in itertools.permutations(nums):
+    for o in itertools.product(ops, repeat=length):
+        for p in itertools.permutations(patterns, length):
+            if [o, n, p] in result: continue
+            f = formula(o, n, p)
+            try:
+                v = eval(f)
+            except ZeroDivisionError: continue 
+            if v != 10: continue
+            result.append([o, n, p])
+            print("%s = %s" % (f, v))
